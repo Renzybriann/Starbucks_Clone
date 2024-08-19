f2a8115c365b866b18c03972f1b1865ed3003ddc
Renzybriann <youngrenzybrian@gmail.com>
2024-08-19 21:15:32 +0800

    for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Starbucks_clone/', // Replace with your repository name
 })
